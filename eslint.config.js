210ffbe9a43f8dd7e1f457ec5060bc82a3ea4a4e
Juga Paazmaya <paazmaya@yahoo.com>
2022-11-20 17:15:29 +0200

    Migrating ESLint to its new flat config style

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import paazmaya from 'eslint-config-paazmaya';
+
+export default [
+
+  paazmaya,
+  {
+    rules: {
+      'no-console': 0,
+      'no-process-env': 0,
+      'no-process-exit': 0
+    }
+  }
+];
